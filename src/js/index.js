e67d8fdfeff49fa4b3a2f815e167af461d43cbb6
dron11231 <astar.dron@gmail.com>
2022-05-29 20:54:46 +0300

    added product card

--- src/js/index.js
+++ src/js/index.js
@@ -24,3 +24,56 @@ const swiper = new Swiper('.swiper', {
     },
   },
 });
+
+const overlay = document.querySelector('.overlay');
+const products = document.querySelectorAll('.product');
+const productCard = document.querySelector('.product-card');
+const closeBtn = document.querySelector('.product-card__btn-close');
+
+products.forEach((product) => {
+  const productsCount = product.querySelector('.product__amount');
+
+  product.addEventListener('click', (e) => {
+    if (e.target.classList.contains('product__btn-inc')) {
+      productsCount.innerHTML++;
+    }
+    if (e.target.classList.contains('product__btn-dec')) {
+      if (productsCount.innerHTML > 1) {
+        productsCount.innerHTML--;
+      }
+    } else if (e.target !== productsCount && !e.target.classList.contains('product__btn')) {
+      productCard.classList.add('product-card--visible');
+      overlay.classList.add('overlay--visible');
+    }
+  });
+});
+closeBtn.addEventListener('click', () => {
+  productCard.classList.remove('product-card--visible');
+  overlay.classList.remove('overlay--visible');
+});
+
+const modalBtnGroups = productCard.querySelectorAll('.product-card__btns');
+const modalOneBtnGroup = modalBtnGroups[0].querySelectorAll('.product-card__btn');
+const modalTwoBtnGroup = modalBtnGroups[1].querySelectorAll('.product-card__btn');
+
+modalOneBtnGroup.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modalOneBtnGroup.forEach((el) => el.classList.remove('product-card__btn--active'));
+    btn.classList.add('product-card__btn--active');
+  });
+});
+
+modalTwoBtnGroup.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modalTwoBtnGroup.forEach((el) => el.classList.remove('product-card__btn--active'));
+    btn.classList.add('product-card__btn--active');
+  });
+});
+
+const ingredients = productCard.querySelector('.ingredients');
+
+ingredients.addEventListener('click', (e) => {
+  if (e.target.classList.contains('ingredients__add-btn')) {
+    e.target.classList.toggle('ingredients__add-btn--active');
+  }
+});
